42014c8176debe4d09cd55a013010e7939d2f758
Eugene Trigubov <trigubov.eugene@gmail.com>
2025-03-16 13:15:35 +0100

    first stage; add code output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,15 @@
-package org.example
+package converter
 
 fun main() {
-    val itemsMap = getItems()
-    showEarnings(itemsMap)
-
-    val totalIncome = getIncome(itemsMap)
-    val (staffExpenses, otherExpenses) = getExpenses()
-    showNetIncome(totalIncome, staffExpenses, otherExpenses)
+    printInfo()
 }
 
-private fun showNetIncome(totalIncome: Int, staffExpenses: Int, otherExpenses: Int) {
-    val netIncome = totalIncome - (staffExpenses + otherExpenses)
-    println("Net income: \$${netIncome}")
+fun printInfo() {
+    println("""
+            145 centimeters is 1.45 meters
+            2 miles is 3.2187 kilometers
+            5.5 inches is 139.7 millimeters
+            12 degrees Celsius is 53.6 degrees Fahrenheit
+            3 pounds is 1.360776 kilograms
+        """.trimIndent())
 }
-
-private fun getExpenses(): Pair<Int, Int> {
-    println("Staff expenses:")
-    val staffExpenses = readln().toInt()
-    println("Other expenses:")
-    val otherExpenses = readln().toInt()
-    return Pair(staffExpenses, otherExpenses)
-}
-
-private fun getIncome(itemsMap: Map<String, Int>): Int {
-    val totalIncome = itemsMap.values.sum();
-    println("\nIncome: \$${totalIncome}")
-    return totalIncome
-}
-
-private fun showEarnings(itemsMap: Map<String, Int>) {
-    println("Earned amount:")
-    itemsMap.forEach { item, amount ->
-        println("$item: \$$amount")
-    }
-}
-
-private fun getItems() = mapOf(
-    "Bubblegum" to 202,
-    "Toffee" to 118,
-    "Ice cream" to 2250,
-    "Milk chocolate" to 1680,
-    "Doughnut" to 1075,
-    "Pancake" to 80,
-)
