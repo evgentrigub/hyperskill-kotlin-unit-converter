febdec6bacc2be1164e3d23832ff21bfcfa93f11
Eugene Trigubov <trigubov.eugene@gmail.com>
2025-03-16 13:25:54 +0100

    fifth stage; add degrees processing; refactor code;

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,52 +21,112 @@ class Converter {
     private fun processInput() {
         print("Enter what you want to convert (or exit): ")
         try {
-            val input = readln().split(" ")
-            if (input[0] == "exit") {
+            val inputString = readln().split(" ")
+            if (inputString[0] == "exit") {
                 isRunning = false
                 return
             }
 
-            numberDouble = input[0].toDouble()
-            val fromUnitInput = input[1]
-            val toUnitInput = input[3]
-
-            fromUnit = getUnitOfMeasure(fromUnitInput)
-            toUnit = getUnitOfMeasure(toUnitInput)
+            setInputData(inputString)
             verifyInputData(fromUnit, toUnit)
-
             convertAndPrintOutput(numberDouble, fromUnit, toUnit)
 
-        } catch (ex: Exception) {
+        } catch (ex: IllegalArgumentException) {
             println(ex.message)
+        } catch (ex: Exception) {
+            println("Parse error\n")
         }
     }
-}
 
-private fun verifyInputData(unitOfMeasureFrom: UnitBase, unitOfMeasureTo: UnitBase) {
-    if (unitOfMeasureFrom == UnitBase.BLANK || unitOfMeasureTo == UnitBase.BLANK) {
-        val fromTxt = if (unitOfMeasureFrom.type == '0') "???" else unitOfMeasureFrom.pluralName
-        val toTxt = if (unitOfMeasureTo.type == '0') "???" else unitOfMeasureTo.pluralName
-        throw Exception("Conversion from $fromTxt to $toTxt is impossible\n")
-    }
+    private fun setInputData(input: List<String>) {
+        try {
+            when (input.size) {
+                4 -> {
+                    fromUnit = getUnitOfMeasure(input[1])
+                    toUnit = getUnitOfMeasure(input[3])
+                }
+                5 -> {
+                    if (checkIfDegree(input[1])){
+                        fromUnit = getUnitOfMeasure("${input[1]} ${input[2]}")
+                        toUnit = getUnitOfMeasure(input[4])
+                    } else if (checkIfDegree(input[3])){
+                        fromUnit = getUnitOfMeasure(input[1])
+                        toUnit = getUnitOfMeasure("${input[3]} ${input[4]}")
+                    }
+                }
+                6 -> {
+                    if (!checkIfDegree(input[1]) || !checkIfDegree(input[4])) throw Exception()
+                    fromUnit = getUnitOfMeasure("${input[1]} ${input[2]}")
+                    toUnit = getUnitOfMeasure("${input[4]} ${input[5]}")
+                }
+                else -> throw Exception()
+            }
 
-    if (unitOfMeasureFrom.type != unitOfMeasureTo.type) {
-        throw Exception("Conversion from ${unitOfMeasureFrom.pluralName} to ${unitOfMeasureTo.pluralName} is impossible\n")
-    }
-}
-
-private fun convertAndPrintOutput(numberDouble: Double, unitOfMeasureFrom: UnitBase, unitOfMeasureTo: UnitBase) {
-    val fromTxt = if (numberDouble != 1.0) unitOfMeasureFrom.pluralName else unitOfMeasureFrom.singularName
-    val outputNum = numberDouble * unitOfMeasureFrom.multiplayer / unitOfMeasureTo.multiplayer
-    val toTxt = if (outputNum != 1.0) unitOfMeasureTo.pluralName else unitOfMeasureTo.singularName
-    println("$numberDouble $fromTxt is $outputNum $toTxt\n")
-}
-
-private fun getUnitOfMeasure(umString: String): UnitBase {
-    for (unit in UnitBase.entries) {
-        if (umString.lowercase().matches(unit.pattern)) {
-            return unit
+            numberDouble = input[0].toDouble()
+            if(numberDouble < 0 && (fromUnit.type == 'L' || fromUnit.type == 'W')) {
+                val unit = if (fromUnit.type == 'L') "Length" else "Weight"
+                throw IllegalArgumentException("$unit shouldn't be negative\n")
+            }
+        } catch (ex: NumberFormatException) {
+            throw IllegalArgumentException("Parse error\n")
         }
     }
-    return UnitBase.BLANK
+
+    private fun checkIfDegree(txt : String) : Boolean {
+        return txt.lowercase().contains("degree")
+    }
+
+    private fun verifyInputData(unitOfMeasureFrom: UnitBase, unitOfMeasureTo: UnitBase) {
+        if (unitOfMeasureFrom == UnitBase.BLANK || unitOfMeasureTo == UnitBase.BLANK) {
+            val fromTxt = if (unitOfMeasureFrom.type == '0') "???" else unitOfMeasureFrom.pluralName
+            val toTxt = if (unitOfMeasureTo.type == '0') "???" else unitOfMeasureTo.pluralName
+            throw IllegalArgumentException("Conversion from $fromTxt to $toTxt is impossible\n")
+        }
+
+        if (unitOfMeasureFrom.type != unitOfMeasureTo.type) {
+            val message = "Conversion from ${unitOfMeasureFrom.pluralName} " +
+                    "to ${unitOfMeasureTo.pluralName} is impossible\n"
+            throw IllegalArgumentException(message)
+        }
+    }
+
+    private fun convertAndPrintOutput(numberDouble: Double, unitOfMeasureFrom: UnitBase, unitOfMeasureTo: UnitBase) {
+        val from = unitOfMeasureFrom.name
+        val to = unitOfMeasureTo.name
+
+        val outputNum =
+            if (unitOfMeasureFrom.type in listOf('W','L'))
+                (numberDouble * unitOfMeasureFrom.multiplayer / unitOfMeasureTo.multiplayer)
+            else calculateDegrees(from, to, numberDouble)
+
+        val fromTxt = if (numberDouble != 1.0) unitOfMeasureFrom.pluralName else unitOfMeasureFrom.singularName
+        val toTxt = if (outputNum != 1.0) unitOfMeasureTo.pluralName else unitOfMeasureTo.singularName
+        println("$numberDouble $fromTxt is $outputNum $toTxt\n")
+    }
+
+    private fun calculateDegrees(
+        from: String,
+        to: String,
+        numberDouble: Double
+    ): Double {
+        return when {
+            from == "Fahrenheit" && to == "Celsius" -> (numberDouble - 32) * 5 / 9
+            from == "Celsius" && to == "Fahrenheit" -> numberDouble * 9 / 5 + 32
+            from == "Celsius" && to == "Kelvin" -> numberDouble + 273.15
+            from == "Kelvin" && to == "Celsius" -> numberDouble - 273.15
+            from == "Kelvin" && to == "Fahrenheit" -> numberDouble * 9 / 5 - 459.67
+            from == "Fahrenheit" && to == "Kelvin" -> (numberDouble + 459.67) * 5 / 9
+            from == to -> numberDouble
+            else -> 0.0
+        }
+    }
+
+    private fun getUnitOfMeasure(umString: String): UnitBase {
+        for (unit in UnitBase.entries) {
+            if (umString.lowercase().matches(unit.pattern)) {
+                return unit
+            }
+        }
+        return UnitBase.BLANK
+    }
 }
--- src/main/kotlin/Unit.kt
+++ src/main/kotlin/Unit.kt
@@ -15,10 +15,19 @@ enum class UnitBase(
     YD('L',"yard", "yards", Regex("yd|yards?"), 0.9144),
     FT('L',"foot", "feet", Regex("ft|foot|feet"), 0.3048),
     IN('L',"inch", "inches", Regex("in|inch(es)?"), 0.0254),
+
     Gram('W', "gram", "grams", Regex("""g|grams?"""),1.0),
     Kilogram('W', "kilogram", "kilograms", Regex("""kg|kilograms?"""),1000.0),
     Milligram('W', "milligram", "milligrams", Regex("""mg|milligrams?"""),0.001),
     Pound('W', "pound", "pounds", Regex("""lb|pounds?"""),453.592),
     Ounce('W', "ounce", "ounces", Regex("""oz|ounce(s)?"""),28.3495),
+
+    Celsius('T', "degree Celsius", "degrees Celsius",
+        Regex("""degrees? celsius|celsius|dc|c"""), 0.0),
+    Fahrenheit('T', "degree Fahrenheit", "degrees Fahrenheit",
+        Regex("""degrees? fahrenheit|fahrenheit|df|f"""), 0.0),
+    Kelvin('T', "kelvin", "kelvins",
+        Regex("""k|kelvins?"""), 0.0),
+
     BLANK('0', "", "", Regex(""),0.0)
 }
